0175c6be3548568c1879281c33af262954dd9ee2
Umer Nasir <unasir@dawaai.pk>
2022-08-30 16:06:00 +0500

    Added PostgreSQL setup.

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,6 +1,20 @@
-export default (_, context) => {
+import { Client } from "https://deno.land/x/postgres@v0.16.1/mod.ts";
 
-    const items = [{ id: 1, name: 'Umer Nasir' }, { id: 2, name: 'Muhammad Ahmed' }];
-    return context.json({ items });
+export default async (_, context) => {
+
+    const client = new Client({
+        user: "postgres",
+        password: "nqi5@j4fPg8WvhJ",
+        database: "postgres",
+        hostname: "db.nycrsvxlzaxsdopkqtlt.supabase.co",
+        port: 6543,
+        tls: { enabled: true }
+    });
+
+    await client.connect();
+
+    const result = await client.queryObject("SELECT * FROM classifications")
+
+    return context.json({ items: result.rows });
 
 }
